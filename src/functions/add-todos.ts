d20efb500d7cc6a9259bc3df03419b4107f9fe42
Paulo Henrique Ferreira de Souza <paulo.souza@cubos.io>
2022-04-21 15:36:13 +0000

    refactor: add todo type

--- src/functions/add-todos.ts
+++ src/functions/add-todos.ts
@@ -1,16 +1,10 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { generateV4 } from "@libs/uuid";
+
 import { document } from "@utils/dynamodbClient";
+import { Todo } from "src/types/Todo";
 
 interface IAddTodosRequest {
-  title: string;
-  done: boolean;
-  deadline: Date;
-}
-
-interface IAddTodosDTO {
-  id: string;
-  user_id: string;
   title: string;
   done: boolean;
   deadline: Date;
@@ -27,7 +21,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     title,
     done,
     deadline,
-  } as IAddTodosDTO;
+  } as Todo;
 
   await document
     .put({
